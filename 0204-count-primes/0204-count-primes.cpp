2b7a935b1bd67c0a2eafb1c882262b0dd5dfc987
Vivek Dagar <vivekdagar2023@gmail.com>
2024-04-27 14:55:11 +0530

    Time: 455 ms (45.13%), Space: 11.3 MB (90.73%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,25 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-	int countPrimes(int n) {
-		if(n <= 2)
-			return 0;
+    static int countPrimes(int n) {
+        if(n <= 2)
+            return 0;
 
-		vector<bool>prime(n, true);
-		prime[0] = prime[1] = false;
+        vector<bool>prime(n+1, true);
+        prime[0] = prime[1] = false;
 
-		for(int p{2}; p * p < n; p++)
-			if(prime.at(p))
-				for(int i{p*p}; i < n; i += p)
-					prime.at(i) = false;
+        for(int p{2}; p * p < n; p++)
+            if(prime.at(p))
+                for(int i{p*p}; i < n; i += p)
+                    prime.at(i) = false;
 
-		// Count primes
-		int count = 0;
-		for (int i = 2; i < n; ++i) {
-			if (prime[i]) {
-				++count;
-			}
-		}
-
-		return count;
-	}
+        return std::count_if(prime.begin() + 2, prime.end()-1,
+                                   [](bool value) { return value; });
+    }
 };
